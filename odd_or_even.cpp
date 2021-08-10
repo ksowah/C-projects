77770133e8b49269e43856dc4620fd03c5d48732
kelvin_xs <80095257+ksowah@users.noreply.github.com>
2021-08-10 00:58:55 +0000

    Add files via upload

---
+++ odd_or_even.cpp
@@ -0,0 +1,23 @@
+
+#include<iostream>
+using namespace std;
+
+
+
+
+int main()
+{
+    int number;
+    cout << "Please enter number: ";
+    cin >> number;
+    cout << endl;
+
+    if(number % 2 == 0){
+        cout << "This is an even number\n";
+    }
+    else{
+        cout << "this is an odd number\n";
+    }
+
+    return 0;
+}
