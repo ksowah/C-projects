2137c8ab244e7ad5e397470a6ad1852a399d4ae0
Kevv_in <80095257+ksowah@users.noreply.github.com>
2021-08-30 13:27:49 +0000

    Update Electricity_charges.cpp

--- Electricity_charges.cpp
+++ Electricity_charges.cpp
@@ -3,56 +3,63 @@
 
 using namespace std;
 
-int main()
+int
+main ()
 {
-    string customerName;
-    double quantityOfElectricity;
-    string category;
-    double monthlyBill;
-    
-    // take user details
-    cout << "please enter your name: ";
-    getline(cin, customerName);
-    cout << "please enter quantity of elcectricity used: ";
-    cin >> quantityOfElectricity;
-    
-    
-    /* categorise the user into domestic, industrial and commercial depending on electricity consumed */
-    if (quantityOfElectricity <= 150){
-        
-        category = "domestic";
-        monthlyBill = quantityOfElectricity * 1.2;
-        
-    }else if(quantityOfElectricity > 350){
-        category = "commercial ";
-        monthlyBill = quantityOfElectricity * 3.5;
-        
-    }else{
-        category = "industrial";
-        monthlyBill = quantityOfElectricity * 2.3;
-        
-    }
-    
-    
-    // Rural electrification charge
-    double ruralElectrification = 0.01 * monthlyBill;
-    
-     monthlyBill += ruralElectrification;
-    
-    // government subsidy
-    double governmentSubsidy = 0.5 * monthlyBill;
-    
-     monthlyBill -= governmentSubsidy;
-    
-    cout << endl << "=================================" << endl;
-    cout << "Name: " << customerName << endl;
-    cout << "Electricity consumed: " << quantityOfElectricity << "kw" << endl;
-    cout << category << " user" << endl;
-    cout << "1% rural electrification: " << ruralElectrification << " GHC" << endl;
-    cout << "50% Government Subsidy: " << governmentSubsidy << " GHC" << endl;
-    cout << "Final balance to be paid: " << monthlyBill << " GHC" << endl;
-    cout << endl << "=================================" << endl;
-    
+  string customerName;
+  double quantityOfElectricity;
+  string category;
+  double monthlyBill;
 
-    return 0;
+  // take user details
+  cout << "please enter your name: ";
+  getline (cin, customerName);
+  cout << "please enter quantity of elcectricity used: ";
+  cin >> quantityOfElectricity;
+
+
+  /* categorise the user into domestic, industrial and commercial depending on electricity consumed */
+  if (quantityOfElectricity <= 150)
+    {
+
+      category = "domestic";
+      monthlyBill = quantityOfElectricity * 1.2;
+
+    }
+  else if (quantityOfElectricity > 350)
+    {
+      category = "commercial ";
+      monthlyBill = quantityOfElectricity * 3.5;
+
+    }
+  else
+    {
+      category = "industrial";
+      monthlyBill = quantityOfElectricity * 2.3;
+
+    }
+
+
+  // Rural electrification charge
+  double ruralElectrification = 0.01 * monthlyBill;
+
+  monthlyBill += ruralElectrification;
+
+  // government subsidy
+  double governmentSubsidy = 0.5 * monthlyBill;
+
+  monthlyBill -= governmentSubsidy;
+
+  cout << endl << "=================================" << endl;
+  cout << "Name: " << customerName << endl;
+  cout << "Electricity consumed: " << quantityOfElectricity << "kw" << endl;
+  cout << category << " user" << endl;
+  cout << "1% rural electrification: " << ruralElectrification << " GHC" <<
+    endl;
+  cout << "50% Government Subsidy: " << governmentSubsidy << " GHC" << endl;
+  cout << "Final balance to be paid: " << monthlyBill << " GHC" << endl;
+  cout << endl << "=================================" << endl;
+
+
+  return 0;
 }
