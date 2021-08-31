a6e03bca63b588aecd3c2b637cdb769ab9e3b6d0
Kevv_in <80095257+ksowah@users.noreply.github.com>
2021-08-31 16:45:10 +0000

    Create secure_shelve.cpp

---
+++ secure_shelve.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    
+  string product;
+  float price;
+  
+  cout << "Enter name of product: ";
+  cin >> product;
+  cout << "Enter price: ";
+  cin >> price;
+  float copyOfPrice = price;
+  
+  float charge = price * 0.02;
+  price += charge;
+  
+  cout << "Shelve secured.\n\n";
+  cout << "Product name: " << product << endl;
+  cout << "Product price: " << copyOfPrice << endl;
+  cout << "2% charge: " << charge << endl;
+  cout << "new price: " << price;
+  
+  
+  
+   
+
+    return 0;
+}
