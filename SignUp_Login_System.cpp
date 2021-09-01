ca40853398efecc18fd427155195bce1b576dc70
Kevv_in <80095257+ksowah@users.noreply.github.com>
2021-09-01 15:37:36 +0000

    Update SignUp_Login_System.cpp

--- SignUp_Login_System.cpp
+++ SignUp_Login_System.cpp
@@ -1,101 +1,54 @@
 #include <iostream>
-#include <fstream>
 #include <string>
+#include <fstream>
 using namespace std;
-
-
-
-
 
 int main()
 {
-
-    // varriables to store user data 
+    
+  string product;
+  float price;
+  
+  int response;
+  
+  cout << "To secure a shelf enter '0'\n";
+  cout << "To read shelf code enter '1'\n";
+  cout << "Enter response: ";
+  cin >> response;
+  cout << endl;
+  
+  if (response == 0){
+      
         
-        string fname;
-        string lname;
-        string userName;
-        string password;
-        string gender;
-        string email;
-        string dateOfBirth;
-        int age;
-        string nationality;
-        int response;
-
-        cout << "To create account enter '1'\n";
-        cout << "To login enter '0'\n";
-        cout << "Enter response: ";
-        cin >> response;
-
-    // conditional statement to put user in controle
-        
-        if (response == 1){
-                
-        //   create and write to file      
-
-            cout << "Please enter first name: ";
-        cin >> fname;
-        cout << "Please enter last name: ";
-        cin >> lname;
-        cout << "Enter user name: ";
-        cin >> userName;
-        cout << "Enter password: ";
-        cin >> password;
-        cout << "Enter gender: ";
-        cin >> gender;
-        cout << "Enter email: ";
-        cin >> email;
-        cout << "Enter date of birth: ";
-        cin >> dateOfBirth;
-        cout << "Enter age: ";
-        cin >> age;
-        cout << "Enter nationality: ";
-        cin >> nationality;
-
-        if(age < 18){
-
-            cout << "Sorry your age is not qualified to be registered";
-
-        }
-
-        ofstream database(userName + password + ".txt");
-
-        database << "First name: " << fname << endl << "Last name: " << lname << endl << "Username: " << userName << endl
-        << "Password: " << password << endl << "Gender: " << gender << endl << "Email: " << email << endl << "Date of Birth: " << dateOfBirth
-        << endl << "Age: " << age << endl << "Nationality: " << nationality << endl;
-
-        cout << "File created successfully";
-
-        database.close();
-
-
-        }else if(response == 0){
-
-                
-       // read file         
-                
-            string myData;
-
-            cout << "Please enter user name: ";
-            cin >> userName;
-            cout << "Please enter password: ";
-            cin >> password;
-                
-                cout << "\n" << + "welcome to your dash board " +  userName << endl;
-                cout << "Your personal data is displayed below\n\n";
-
-            ifstream login(userName + password + ".txt");
-
-            while (getline(login, myData)){
-
-                cout << myData << endl;
-
-            }
-
-            login.close();
-        }
-
+  cout << "Enter name of product: ";
+  cin >> product;
+  cout << "Enter price: ";
+  cin >> price;
+  
+  
+   float copyOfPrice = price;
+  
+  float charge = price * 0.02;
+  price += charge;
+  
+  ofstream item(product + ".txt");
+  
+  item << "=========================================" << endl <<
+  "\nYOUR SHELVE HAS BEEN SECURED\n" << endl << "product name: " << product << endl <<
+  "Product price: " << copyOfPrice << endl << "2% charge: " << charge << endl << "new price: " << price <<
+  endl << "\n=========================================";
+  
+  item.close();
+  
+  cout << "\nShelve successfully secured";
+      
+  }else if(response == 1){
+      
+      ifstream readitem(product + ".txt"); 
+      
+  }
+  
+   
 
     return 0;
 }
